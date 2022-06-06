0cd24a838dfab4363dbc5f258d8eedb4220c6c76
sunny <guptasunny3905@gmail.com>
2022-06-06 03:07:50 -0400

    new

---
+++ routes/student.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req)
+  res.json({name :"My name is Sunny gupta "})
+  res.render('index', { title: 'Sunny' });
+});
+router.get('/:da-:ta-:ch', function(req, res, next) {
+  console.log(req.params.da)
+  res.json({name :req.params.da+","+req.params.ch +" and "+req.params.ta})
+});
+
+router.get('/:da', function(req, res, next) {
+  console.log(req.params.da)
+  res.json({name:req.params.da})
+  
+});
+
+module.exports = router;
